79db3472bf361e48172f410922442297e5b27835
Mihir Singh <mihirs16@gmail.com>
2026-05-02 23:21:52 +0100

    feat: clock and weather island

--- systatus/src/index.tsx
+++ systatus/src/index.tsx
@@ -7,67 +7,90 @@ import * as zebar from 'zebar';
 const providers = zebar.createProviderGroup({
   cpu: { type: 'cpu' },
   memory: { type: 'memory' },
-  weather: { type: 'weather' },
+  weather: { type: 'weather', latitude: 51.5072, longitude: 0.1276 },
+  date: { type: 'date' }
 });
 
 render(() => <App />, document.getElementById('root')!);
 
 function App() {
-  const [output, setOutput] = createStore(providers.outputMap);
+    const [output, setOutput] = createStore(providers.outputMap);
 
-  providers.onOutput(outputMap => setOutput(outputMap));
+    providers.onOutput(outputMap => setOutput(outputMap));
 
-  return (
-    <div class="app">
-        <div class="archipelago-left"></div>
-        <div class="archipelago-center"></div>
+    return (
+        <div class="app">
+            <div class="archipelago-left"></div>
+            <div class="archipelago-center"></div>
        
-        {/* Top-right group of islands */}
-        <div class="archipelago-right">
+            {/* Top-right group of islands */}
+            <div class="archipelago-right">
 
-            {/* Island for CPU/Mem stats */}
-            <div class="island">
-                <div class="stat">
-                    <span class="icon">
-                        <i class="nf nf-oct-cpu"></i> 
-                    </span>
-                    <span class="value">
-                        {
-                            output.cpu ? 
-                                Math.round(output.cpu.usage) : '--'
-                        }%
-                    </span>
-                </div>
-                <span class="stat-divider">·</span>
-                <div class="stat">
-                    <span class="icon">
-                    <i class="nf nf-fa-memory"></i> 
-                    </span>
-                    <span class="value">
-                        {
-                            output.memory ? 
-                                Math.round(output.memory.usage) : '--'
-                        }%
-                    </span>
-                </div>
-            </div>
-
-            {/* Island for Weather */}
-            { output.weather &&
+                {/* Island for CPU/Mem stats */}
                 <div class="island">
                     <div class="stat">
-                        <span class="icon weather-icon">
-                            {getWeatherIcon(output.weather.status)}
+                        <span class="icon">
+                            <i class="nf nf-oct-cpu"></i> 
                         </span>
                         <span class="value">
-                            {Math.round(output.weather.celsiusTemp)}°C
+                            {
+                                output.cpu ? 
+                                    Math.round(output.cpu.usage) : '--'
+                            }%
+                        </span>
+                    </div>
+                    <span class="stat-divider">·</span>
+                    <div class="stat">
+                        <span class="icon">
+                            <i class="nf nf-fa-memory"></i> 
+                        </span>
+                        <span class="value">
+                            {
+                                output.memory ? 
+                                    Math.round(output.memory.usage) : '--'
+                            }%
                         </span>
                     </div>
                 </div>
-            }
-        </div> 
-    </div>
-  );
+
+                {/* Island for Weather and Datetime */}
+                <div class="island">
+                    {
+                        output.weather && 
+                        <>
+                            <div class="stat">
+                                <span class="icon weather-icon">
+                                    {getWeatherIcon(output.weather.status)}
+                                </span>
+                                <span class="value muted">
+                                    {Math.round(output.weather.celsiusTemp)}°C
+                                </span>
+                            </div>
+                            <span class="stat-divider">|</span>
+                        </>
+                    }
+                    <div class="stat">
+                        <span class="value">
+                            {
+                                output.date && 
+                                    getFormattedDt(output.date.now).d
+                            }
+                        </span>
+                    </div>
+                    <span class="stat-divider">·</span>
+                    <div class="stat">
+                        <span class="value">
+                            {
+                                output.date && 
+                                    getFormattedDt(output.date.now).t
+                            }
+                        </span>
+                    </div>
+
+                </div>
+            </div> 
+        </div>
+    );
 }
 
 function getWeatherIcon(status: string) {
@@ -92,4 +115,19 @@ function getWeatherIcon(status: string) {
     }
 
     return (<i class={`nf ${weatherIconClass}`}></i>);
+}
+
+function getFormattedDt(epoch: number) {
+    const dt = new Date(epoch);
+    return {
+        t: dt.toLocaleTimeString([], { 
+            hour: '2-digit', 
+            minute: '2-digit', 
+            hour12: false 
+        }),
+        d: dt.toLocaleDateString([], { 
+            day: '2-digit', 
+            month: 'short' 
+        })
+    }
 }
